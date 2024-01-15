965218075fb48fada2e16289b78356949ce00db0
IKS-cod <ik.smirnov@yandex.ru>
2024-01-15 17:55:05 +0300

    Исправил ДЗ 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.out.println("Задание1");
         var dog = 8.0;
         System.out.println(dog);
